2e8c76a9702be5e6ba776cc877bf8d9fe5d0ca35
Carlos Rodriguez <c.rodriguez@arduino.cc>
2017-08-15 11:02:56 +0200

    add images to examples

--- LCR.h
+++ LCR.h
@@ -8,16 +8,16 @@
 
 // Musical Notes
 #define C  	523
-#define CS 	554
+#define C# 	554
 #define D		587
-#define DS		622
+#define D#		622
 #define E		659
 #define F		698
-#define FS		739
+#define F#		739
 #define G		783
-#define GS	830
+#define G#	830
 #define A		880
-#define AS		932
+#define A#		932
 #define B		987
 
 //Movement direcction
